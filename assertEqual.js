63bf79e8706bd47a6506ec408c5dcbc00cc8a5c8
Saba Oji <sabaoji@gmail.com>
2023-04-23 13:36:00 -0400

    Add assertEqual.js file

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+
+console.log(assertEqual);
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
