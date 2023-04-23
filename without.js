b8c7c9cf03648dd8bbd4045527fd0344e796e804
Saba Oji <sabaoji@gmail.com>
2023-04-23 17:44:53 -0400

    without example, finding the difference between the two arrays

---
+++ without.js
@@ -0,0 +1,43 @@
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const without = function(source, itemsToRemove) {
+  let output = [];
+  for (let i = 0; i < source.length; i++) {
+    let match = false;
+    for (let j = 0; j < itemsToRemove.length; j++) {
+      if (source[i] === itemsToRemove[j]) {
+        match = true;
+      }
+    }
+    if (!match) {
+      output.push(source[i]);
+    }
+  }
+  return output;
+};
+
+const words = ["hello", "world", "lighthouse"];
+const withoutResult = without(words, ["lighthouse"]);
+assertArraysEqual(withoutResult, ["hello", "world"]);
+console.log(withoutResult);
