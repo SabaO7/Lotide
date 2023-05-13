25ececccbcb872dc03081125dc47c5285fda29ef
Saba Oji <sabaoji@gmail.com>
2023-05-13 19:18:49 -0400

    refactoring

--- middle.js
+++ middle.js
@@ -1,25 +1,3 @@
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-
-  return true;
-};
-
 const middle = function(arr) {
   if (arr.length <= 2) {
     return [];
@@ -32,7 +10,5 @@ const middle = function(arr) {
   }
 };
 
-console.log(middle([1, 2]));
-console.log(middle([1, 2, 3, 4, 5]));
-console.log(middle([1, 2, 3, 4, 5, 6, "7"]));
-console.log(middle([1, 2, 3, 4, 5, 6]));
+module.exports = middle;
+
