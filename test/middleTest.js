194493645bb08c6e6e15f71c0618fbdc5113010c
Saba Oji <sabaoji@gmail.com>
2023-05-13 19:19:23 -0400

    refactoring

---
+++ test/middleTest.js
@@ -0,0 +1,8 @@
+const middle = require('../middle');
+const assertArraysEqual = require('../assertArraysEqual');
+
+
+console.log(middle([1, 2]));
+console.log(middle([1, 2, 3, 4, 5]));
+console.log(middle([1, 2, 3, 4, 5, 6, "7"]));
+console.log(middle([1, 2, 3, 4, 5, 6]));
