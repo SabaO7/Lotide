c682e7edc52d28729c78b118a0cdcee42c806e56
Saba Oji <sabaoji@gmail.com>
2023-05-13 19:36:08 -0400

    unit testing

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,10 +1,15 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
+describe("#tail", () => {
+  it("should return an empty array for an empty array", () => {
+    assert.deepEqual(tail([]), []);
+  });
 
-console.log(assertEqual);
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
+  it("should return the tail (all elements except the first) of the array", () => {
+    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    assert.strictEqual(result.length, 2);
+    assert.strictEqual(result[0], "Lighthouse");
+    assert.strictEqual(result[1], "Labs");
+  });
+});
