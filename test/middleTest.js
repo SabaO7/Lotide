9bff6c15dd44e15108c44f35eb5f6118f3d16222
Saba Oji <sabaoji@gmail.com>
2023-05-13 19:34:36 -0400

    unit testing

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,16 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
 
+describe("#middle", () => {
+  it("should return an empty array for an array with 2 or fewer elements", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
 
-console.log(middle([1, 2]));
-console.log(middle([1, 2, 3, 4, 5]));
-console.log(middle([1, 2, 3, 4, 5, 6, "7"]));
-console.log(middle([1, 2, 3, 4, 5, 6]));
+  it("should return the middle element for an array with odd length", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+
+  it("should return the middle two elements for an array with even length", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+});
