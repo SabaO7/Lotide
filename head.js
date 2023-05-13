65ebae601e0db1b55f9df7afbbdbc5c79c2983eb
Saba Oji <sabaoji@gmail.com>
2023-05-13 18:42:40 -0400

    withtout the testing code at the end

--- head.js
+++ head.js
@@ -2,16 +2,7 @@ const head = function(array) {
   return array[0];
 };
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} ===  ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+
+const assertEqual = require('./assertEqual');
 
 
-console.log(assertEqual);
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
